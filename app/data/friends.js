35256f46939f305619d7d8219837fad33d3a9297
erithr <hvillalo@linkedin.biz>
2018-12-06 15:11:04 -0600

    made changes to friends array changing it from json object to js object. changed code to reflect changes

---
+++ app/data/friends.js
@@ -0,0 +1,67 @@
+const frienArr = [
+    {
+        name: "erithr",
+        photo: "https://memegenerator.net/img/instances/69962947.jpg",
+        scores: [
+            "2",
+            "3",
+            "5",
+            "3",
+            "2",
+            "1",
+            "1",
+            "2",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Dio",
+        photo: "https://memegenerator.net/img/instances/69962947.jpg",
+        scores: [
+            "2",
+            "3",
+            "5",
+            "3",
+            "2",
+            "1",
+            "1",
+            "2",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Dio",
+        photo: "https://memegenerator.net/img/instances/69962947.jpg",
+        scores: [
+            "2",
+            "3",
+            "5",
+            "3",
+            "2",
+            "1",
+            "1",
+            "2",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Janet",
+        photo: "http://picture-cdn.wheretoget.it/fpsdx1-l-610x610-hair+accessory-rocky+horror+picture-janet+weiss-white.jpg",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "2"
+        ]
+    }
+]
+module.exports = frienArr;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,41 +1,35 @@
 // Routes
 // =============================================================
+const friendArr = require('../data/friends');
 module.exports = function apiRoutes(app) {
-    const fs = require("fs");
-    const path = require("path");
-    var friends = require("../data/friends.json");
+    // const friends = require("../data/friends.js");
 
     app.get("/api/friends", function (req, res) {
-        return res.json(friends);
+        return res.json(friendArr);
     }); //get /api/friends
 
     app.post("/api/friends", function (req, res) {
-        var totDiff;
-        var diffArry = [];
-        var newFriend = req.body;
+        const match = findmatch(req.body, friendArr);
 
-        for (var i = 0; i < friends.length; i++) {
-            totDiff = 0;
-            for (var j = 0; j < newFriend.scores.length; j++) {
-                totDiff += Math.abs(friends[i].scores[j] - newFriend.scores[j]);
-            } //for j
-            diffArry.push(totDiff);
-        } //for i
+        friendArr.push(req.body);
+        res.json(match);
+    });
+    const findmatch = (user, friendArr) => {
+        let bestMatch = {};
 
-        var match = diffArry.indexOf(Math.min(...diffArry));
+        let differece = 0;
+        friendArr.forEach((friend, friendIndex) => {
+            let answerDifference = 0;
+            friend.scores.forEach((scores, index) => answerDifference += Math.abs(+scores - +user.scores[index]));
+            if (answerDifference < differece) {
+                bestMatch = friend;
+            } else if (friendIndex == 0) {
+                bestMatch = friend;
+            };
+            differece = answerDifference;
+        });
+        console.log(bestMatch)
+        return bestMatch;
+    };
 
-        friends.push(newFriend);
-
-        console.log(newFriend);
-
-        fs.readFile(path.join(__dirname, "../data/friends.json"), "utf8", function (err, data) {
-            if (err) throw err;
-            var json = JSON.parse(data);
-            json.push(newFriend);
-            fs.writeFile(path.join(__dirname, "../data/friends.json"), JSON.stringify(json, null, 2), function (err) {
-                if (err) throw err;
-            });
-        }); //fs.readFile
-        res.json(friends[match]);
-    }); //post /api/friends
-} //module.exports
+};
